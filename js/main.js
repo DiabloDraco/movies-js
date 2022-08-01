27be3a097ed13df64d6885416883195f0f1e4d63
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2022-08-01 13:52:16 +0500

    added

--- js/main.js
+++ js/main.js
@@ -1,29 +1,17 @@
 elMovies = movies.slice(0 , 150)
+elSelect = document.querySelector("#selects")
 
+let wArray = elMovies.map(item =>{
+    return {
+        title:item.Title.toString(),
+        videoUrl:`https://www.youtube.com/watch?v=${item.ytid}`,
+        categories:item.Categories.split("|"),
+        movieYear:item.movie_year,
+        rating:item.imdb_rating,
+        image:`https://i.ytimg.com/vi/${item.ytid}/mqdefault.jpg`
+    }
+})
 
-function normolize(array) {
-    let newArray = [];
-
-
-    array.forEach(item => {
-        let newObject = {}
-
-        newObject.title = item.Title.toString();
-        newObject.videoUrl = `https://www.youtube.com/watch?v=${item.ytid}`;
-        newObject.categories = item.Categories.split("|");
-        newObject.movieYear = item.movie_year;
-        newObject.rating = item.imdb_rating;
-        newObject.image = `https://i.ytimg.com/vi/${item.ytid}/mqdefault.jpg`;
-
-        newArray.push(newObject)
-    });
-
-
-
-    return newArray
-}
-
-let wArray = normolize(elMovies);
 let elMovieWrapper = document.querySelector(".movie__wrapper");
 let elTemplate = document.querySelector("#movie_card").content;
 let elReslut = document.querySelector("#results")
@@ -31,22 +19,23 @@ function renderMovies(array, wrapper) {
     wrapper.innerHTML = null
     elReslut.textContent = array.length
     let tempFragment = document.createDocumentFragment()
-
+    
     for (const item of array) {
         let templateItem = elTemplate.cloneNode(true)
-
+        
         templateItem.querySelector(".movie__img").src = item.image;
         templateItem.querySelector(".movie__title").textContent = item.title;
         templateItem.querySelector(".movie__year").textContent = item.movieYear;
         templateItem.querySelector(".movie__rating").textContent = item.rating;
         templateItem.querySelector(".movie__url").href = item.videoUrl;
-
-
+        templateItem.querySelector("#categories").textContent = item.categories;
+        
+        
         tempFragment.appendChild(templateItem)
     }
-
+    
     wrapper.appendChild(tempFragment)
-
+    
 }
 
 renderMovies(wArray, elMovieWrapper);
@@ -64,3 +53,28 @@ elForm.addEventListener("submit" , function (evt) {
     }
     renderMovies(FindedArray , elMovieWrapper)
 })
+
+function getCategories(array) {
+    let categoriesArray = []
+    for (const item of array) {
+        for (const itemCategories of item.categories) {
+            if (!categoriesArray.includes(itemCategories)) {
+                categoriesArray.push(itemCategories)
+            }
+        }
+    }
+    return categoriesArray
+}
+
+let allCategories = getCategories(wArray)
+
+function categoriesRender(array) {
+    for (let i = 0; i < array.length; i++) {
+        let newOption = document.createElement("option")
+        newOption.value = array[i]
+        newOption.textContent = array[i]
+        elSelect.appendChild(newOption)
+    }
+}
+
+categoriesRender(allCategories)
